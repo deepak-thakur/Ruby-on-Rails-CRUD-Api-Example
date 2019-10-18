fe5b089f79f8db6157f4c34a795898e939d8a130
Deepak Thakur <sw215@enovate-it.com>
2019-10-18 15:12:59 +0530

    all crud api example

--- app/controllers/Api/v1/articles_controller.rb
+++ app/controllers/Api/v1/articles_controller.rb
@@ -3,20 +3,52 @@ module Api
     class ArticlesController < ApplicationController
         def index
          @articles = Article.order('created_at DESC');
-         render json:{status:'SUCCESS', message:'OK',data:@articles},status: :ok
+         render json:{status:'SUCCESS', message:'OK messages loaded',data:@articles},status: :ok
 
         end 
+
+        def show
+           @article = Article.find(params[:id]);
+         render json:{status:'SUCCESS', message:'OK article loaded',data:@article},status: :ok
+
+        end
+
+        def create 
+            @article = Article.new(article_params)
+            
+            if @article.save
+         render json:{status:'SUCCESS', message:'OK article is saved',data:@article},status: :ok
+            else
+         render json:{status:'Failed', message:'Article is not saved',data:@article.errors},status: :unprocessable_entity
+           
+        end
+   end
+
+         def destroy 
+            @article = Article.find(params[:id])
+            @article.destroy()
+            render json:{status:'SUCCESS', message:'OK article is deleted',data:@article},status: :ok
+         end
+
+
+         def update
+            @article = Article.find(params[:id])
+            if @article.update_attributes(article_params)
+               render json:{status:'SUCCESS', message:'OK article is updated',data:@article},status: :ok
+               else
+         render json:{status:'Failed', message:'Article is not updated',data:@article.errors},status: :unprocessable_entity
+
+               end   
+               
+         
+         end   
+
+    
+         private
+    def article_params
+       params.require(:article).permit(:title, :body)   
+    
     end
   end 
 end
-
-# module Api
-#     module V1
-#         class ArticlesController < ApplicationController
-#             def index
-#                 @articles = Article.order('created_at DESC')
-#             end
-        
-#     end
-    
-# end
+end
